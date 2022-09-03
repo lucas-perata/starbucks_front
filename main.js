84444a609e8670701993e55cd1b3da2b098151c5
lucas-perata <contacto.perata@gmail.com>
2022-09-03 00:06:13 -0300

    Prettier estilo

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const menu = document.querySelector(".menu")
-const navButtons = document.querySelector(".nav-button")
-const hamburger= document.querySelector(".hamburger")
-const closeIcon= document.querySelector(".close-icon");
+const menu = document.querySelector(".menu");
+const navButtons = document.querySelector(".nav-button");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".close-icon");
 const menuIcon = document.querySelector(".menu-icon");
 
-function toggleMenu(){
-    if (menu.classList.contains("show-menu")){
-        menu.classList.remove("show-menu")
-        closeIcon.style.display = "none"
-        menuIcon.style.display = "block"
-    }
-    else {
-        menu.classList.add("show-menu")
-        closeIcon.style.display = "block";
-        closeIcon.style.visibility = "visible";
-        closeIcon.style.position = "relative"
-        menuIcon.style.display = "none";
-    }
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("show-menu");
+    closeIcon.style.display = "block";
+    closeIcon.style.visibility = "visible";
+    closeIcon.style.position = "relative";
+    menuIcon.style.display = "none";
+  }
 }
 
-hamburger.addEventListener("click", toggleMenu)
+hamburger.addEventListener("click", toggleMenu);
